b09cce42dd7455ce8ff56b2869d90c186090e457
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-10 22:00:57 -0600

    added routes to display html files

--- server.js
+++ server.js
@@ -9,20 +9,43 @@ const PORT = process.env.PORT || 3030
 // CREATING INSTANCE OF EXPRESS APP
 const app = express();
 
-app.use(express.static("public"));
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// ROUTES TO DISPLAY HTML PAGES
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  app.get("/exercise", (req,res) => {
+      res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+  });
+  
+  app.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+  });
+
+
+// app.use(require("./routes/api.js"));
+
+// app.use(logger("dev"));
 
 // NAMING DATABASE & URL
-const databaseUrl = "workoutdb";
-const collections = ["exercises"];
-const db = mongojs(databaseUrl, collections);
+// const databaseUrl = "workoutdb";
+// const collections = ["exercises"];
+// const db = mongojs(databaseUrl, collections);
 
-// ALERTING IF DB ERROR
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
+// // ALERTING IF DB ERROR
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname + "./public/index.html"));
@@ -117,7 +140,6 @@ db.on("error", error => {
 
 
 
-app.listen(3030, () => {
-    console.log("App running on port 3030!");
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
   });
-  
